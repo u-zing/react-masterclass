2b8637e0588cfc8f9b977596eaa05151f68f1234
u-zing <dbwls927@gmail.com>
2025-04-05 16:07:03 +0900

    [feat] : 스타일 테마 설정 및 타입 선언 추가
    - styled.d.ts 파일 생성하여 Theme 타입 선언
    - theme.ts 파일 생성 후 기본 테마 구성
    - index.tsx에서 ThemeProvider로 테마 주입
    - App.tsx에서 props로 theme 활용

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+//테마 생성(다크모드,라이트모드)
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+};
+export const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+};
